cd35c13f6f1b8af3edf4b4c40dc5c7277451c081
Arvind Vyas <arvindvyas07@gmail.com>
2023-08-25 05:23:06 +0530

    will print different data as each module in node has different scope

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const add = require("./add.js")
-
-console.log("Hellow from index.jse")
-
-const sum = add(3,4)
-const sum2 = add(3,4)
-console.log(sum);
-console.log(sum2)
+require("./batman")
+require("./superman")
