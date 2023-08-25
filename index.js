15f4b6dbc193af4abf74836dd1f5d73216b05c21
Arvind Vyas <arvindvyas07@gmail.com>
2023-08-25 07:07:57 +0530

    module caching

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-require("./batman")
-require("./superman")
+const superHero = require("./super-hero")
+
+console.log(superHero.getName());
+superHero.setName("Superman");
+console.log(superHero.getName());
+
+const newSuperHero = require("./super-hero")
+console.log(newSuperHero.getName());
