c7c4b8a4e3d589a69529e8b9cec1493c4f9cd3a8
Arvind Vyas <arvindvyas07@gmail.com>
2023-08-25 07:26:01 +0530

    how to export 5

--- math.js
+++ math.js
@@ -1,12 +1,12 @@
- const add = (a,b) => {
+  module.exports.add = (a,b) => {
     return a + b;
  }
 
- const substract = (a,b) =>{
+ module.exports.substract = (a,b) =>{
     return a -b;
  }
 
- module.exports = {
-     add,
-     substract,
- };
+//  module.exports = {
+//      add,
+//      substract,
+//  };
