82345aa963b00309f25d01c439383103f05f8733
Arvind Vyas <arvindvyas07@gmail.com>
2023-08-26 16:14:12 +0530

    Add json file and print the attributes

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const math  = require('./math')
-const {add, substract} = math
-console.log(add(3,5));
-console.log(substract(3,5));
+// const math  = require('./math')
+// const {add, substract} = math
+// console.log(add(3,5));
+// console.log(substract(3,5));
+
+
+const data = require("./data.json");
+
+console.log(data.name);
+console.log(data["address"]["street"]);
