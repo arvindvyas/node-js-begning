f758c68b2e95263afcb7f2de82f4062a72c8c362
Arvind Vyas <arvindvyas07@gmail.com>
2023-08-27 06:11:06 +0530

    Added callbacks and type of callbacks

--- index.js
+++ index.js
@@ -14,34 +14,53 @@
 // node prefix is optional we can also call 
 // const path = require("node:path")
 // node prefix is optional we can also call 
-const path = require("path");
+// const path = require("path");
 
-console.log(__filename);
-console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
 
-console.log(path.basename(__filename));
-console.log(path.basename(__dirname));
+// console.log(path.basename(__filename));
+// console.log(path.basename(__dirname));
 
-console.log(path.extname(__filename));
-console.log(path.extname(__dirname));
+// console.log(path.extname(__filename));
+// console.log(path.extname(__dirname));
 
-console.log(path.parse(__filename));
+// console.log(path.parse(__filename));
 
-console.log(path.format(path.parse(__filename)));
+// console.log(path.format(path.parse(__filename)));
 
-console.log(path.isAbsolute(__filename));
-console.log(path.isAbsolute('./data.json'));
+// console.log(path.isAbsolute(__filename));
+// console.log(path.isAbsolute('./data.json'));
 
-console.log(path.join("folder1", "folder2", "index.html"));
+// console.log(path.join("folder1", "folder2", "index.html"));
 
-console.log(path.join("/folder1", "folder2", "index.html"));
-console.log(path.join("/folder1", "/folder2", "/index.html"));
-console.log(path.join("/folder1", "folder2", "../index.html"));
-console.log(path.join(__dirname, "data.json"));
+// console.log(path.join("/folder1", "folder2", "index.html"));
+// console.log(path.join("/folder1", "/folder2", "/index.html"));
+// console.log(path.join("/folder1", "folder2", "../index.html"));
+// console.log(path.join(__dirname, "data.json"));
 
+// console.log(path.resolve("/folder1", "folder2", "index.html"));
+// console.log(path.resolve("/folder1", "/folder2", "/index.html"));
+// console.log(path.resolve("/folder1", "folder2", "../index.html"));
+// console.log(path.resolve(__dirname, "data.json"));
 
+// *** callbacks ****
+// - Funcation are first class objects 
+// - Funcation can pass as argument to the function.
+// - Funcation can return as value from other funcation.
 
-console.log(path.resolve("/folder1", "folder2", "index.html"));
-console.log(path.resolve("/folder1", "/folder2", "/index.html"));
-console.log(path.resolve("/folder1", "folder2", "../index.html"));
-console.log(path.resolve(__dirname, "data.json"));
+function greet(name){
+    console.log(`Hello ${name}`)
+}
+
+function greetArvind(greetFn){
+    const name = "Arvind";
+    greetFn(name);
+}
+greetArvind(greet);
+
+function higherOrderFuncation(callback){
+    const name = "Arvind Vyas";
+    callback(name);
+}
+higherOrderFuncation(greet);
