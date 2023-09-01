5908c3d884648152934e9727812f4416aeed899d
Arvind Vyas <arvindvyas07@gmail.com>
2023-09-01 17:08:22 +0530

    Added await function to print data one by one

--- 7hours/index.js
+++ 7hours/index.js
@@ -1,27 +1,44 @@
-const fs = require("fs");
+const fsPromises = require("fs").promises;
+const { writeFile } = require("fs");
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'starter.txt'),'utf-8', (err, data)=>{
-  if (err) throw err;
-    console.log(data);  //if you are not using utf-8 you can also use toString() method to convert buffer object to actual text
-})
+const fileOps = async ()=>{
+    try {
+      const data = await fsPromises.readFile(path.join(__dirname, 'starter.txt'), 'utf-8');
+      console.log(data);
+      await fsPromises.writeFile(path.join(__dirname, 'promiseWrite.txt'), data);
+      await fsPromises.appendFile(path.join(__dirname, 'promiseWrite.txt'), '\n\n Looks I am an expert now');
+      await fsPromises.rename(path.join(__dirname, 'promiseWrite.txt'),path.join(__dirname, 'promiseComplete.txt'));
+      const newData = await fsPromises.readFile(path.join(__dirname, 'promiseComplete.txt'), 'utf-8');
+      console.log(newData);
+    } catch(err){
+        console.error(err);
+    }
+}
 
-console.log("Hello ... !!");
-// exit on uncought error
+fileOps();
 
-fs.writeFile(path.join(__dirname, 'reply.txt'),'Nice to meet you Mr NODE', (err)=>{
-    if (err) throw err;
-      console.log("write complete"); 
-})
+// fsPromises.readFile(path.join(__dirname, 'starter.txt'),'utf-8', (err, data)=>{
+//   if (err) throw err;
+//     console.log(data);  //if you are not using utf-8 you can also use toString() method to convert buffer object to actual text
+// })
 
-fs.appendFile(path.join(__dirname, 'text.txt'),'I am learning node', (err)=>{
-    if (err) throw err;
-      console.log("append complete"); 
-})
-  
-  
 
-process.on("uncoughtException", err=>{
-  console.log(`There was an uncought error: ${err}`);
-  process.exit(1);
-})
+// console.log("Hello ... !!");
+// // exit on uncought error
+
+// fs.writeFile(path.join(__dirname, 'reply.txt'),'Nice to meet you Mr NODE', (err)=>{
+//     if (err) throw err;
+//     console.log("write complete"); 
+
+//     fs.appendFile(path.join(__dirname, 'reply.txt'),'\n\nI am learning node', (err)=>{
+//         if (err) throw err;
+//         console.log("Append complete"); 
+//     })
+      
+// })
+
+// process.on("uncoughtException", err=>{
+//   console.log(`There was an uncought error: ${err}`);
+//   process.exit(1);
+// })
