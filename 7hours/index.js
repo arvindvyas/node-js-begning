b5cdf01755c4576b4cb36f72059f5fed041327f4
Arvind Vyas <arvindvyas07@gmail.com>
2023-09-01 16:02:08 +0530

    Added path , added write method

--- 7hours/index.js
+++ 7hours/index.js
@@ -1,6 +1,21 @@
 const fs = require("fs");
+const path = require('path');
 
-fs.readFile("././starter.txt",'utf-8', (err, data)=>{
+fs.readFile(path.join(__dirname, 'starter.txt'),'utf-8', (err, data)=>{
   if (err) throw err;
-    // console.log(data);  if you are not using utf-8 you can also use toString() method to convert buffer object to actual text
+    console.log(data);  //if you are not using utf-8 you can also use toString() method to convert buffer object to actual text
+})
+
+console.log("Hello ... !!");
+// exit on uncought error
+
+fs.writeFile(path.join(__dirname, 'reply.txt'),'Nice to meet you Mr NODE', (err)=>{
+    if (err) throw err;
+      console.log("write complete"); 
+  })
+  
+
+process.on("uncoughtException", err=>{
+  console.log(`There was an uncought error: ${err}`);
+  process.exit(1);
 })
